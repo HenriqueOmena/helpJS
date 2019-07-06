88d13dae6d16a756eb4dce889ec47a8d590f977c
Henrique Omena <88lmao@gmail.com>
2019-07-06 17:03:31 +0100

    add some animation to my list

--- main.js
+++ main.js
@@ -5,22 +5,64 @@ class Menu {
 
         this.list = document.querySelectorAll('li')
 
-        this.hideCodes();
+        this.init();
+
     }
 
-    hideCodes() {
-        anime({
-            targets: 'div',
-            translateX: 250,
-            rotate: '1turn',
-            backgroundColor: 'red',
-            duration: 6800
-          });
+    init () {
+        //just add none for all elements
         this.codes.forEach( el => {
+            el.style.display = 'none';
+        })
 
+        this.list.forEach( el => {
+            const preEl = el.nextSibling.nextSibling
+            this.showHideCode(el, preEl)
         })
     }
+    animeCode(element) {
+        anime({
+            targets: element,
+            left: '40px',
+            backgroundColor: 'red',
+            borderRadius: ['0%', '10%'],
+            easing: 'easeInOutQuad'
+        });
+    }
+    animeLi(element, status = true) {
+        if (status) {
+            anime({
+                targets: element,
+                translateX: [10, 750], // from 100 to 250
+                borderRadius: ['0%', '40%'],
+                easing: 'easeInOutQuad'
+            });
+        } else {
+            anime({
+                targets: element,
+                translateX: [750, 0], // from 100 to 250
+                borderRadius: ['0%', '0%'],
+                easing: 'easeInOutQuad'
+            });
+        }
 
+    }
+
+
+    showHideCode(clickEl, showEL) {
+        console.log(clickEl);
+        clickEl.addEventListener('click', () => {
+            if (showEL.style.display === 'block') {
+                //this.animationOut(showEL)
+                this.animeLi(clickEl, false)
+                showEL.style.display = 'none';
+            } else {
+                showEL.style.display = 'block';
+                //this.animeCode(showEL)
+                this.animeLi(clickEl)
+            }
+        })
+    }
 }
 
 
